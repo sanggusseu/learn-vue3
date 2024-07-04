93320ffa332ebaaeac22c2d34b1b27b5e08c2cb3
sanggusseu <sanggusseu@gmail.com>
2024-07-04 19:47:00 +0900

    Composition API

---
+++ src/lectures/4/App_CompositionAPI.vue
@@ -0,0 +1,37 @@
+<template>
+	<div>
+		<h2>반응형 메시지</h2>
+		<p>{{ reactiveMessage }}</p>
+		<button v-on:click="addReactiveMessage">Add Message</button>
+		<h2>일반 메시지</h2>
+		<p>{{ normalMessage }}</p>
+		<button v-on:click="addNormalMessage">Add Message</button>
+	</div>
+</template>
+
+<script>
+import { isRef, ref } from 'vue';
+
+export default {
+	setup() {
+		const reactiveMessage = ref('Hello Reactive Message');
+		const addReactiveMessage = () => {
+			reactiveMessage.value = reactiveMessage.value + '!';
+		};
+		console.log('isRef(reactiveMessage): ', isRef(reactiveMessage));
+		let normalMessage = 'Hello Normal Message';
+		const addNormalMessage = () => {
+			normalMessage = normalMessage + '!';
+		};
+		console.log('isRef(normalMessage): ', isRef(normalMessage));
+		return {
+			reactiveMessage,
+			addReactiveMessage,
+			normalMessage,
+			addNormalMessage,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
