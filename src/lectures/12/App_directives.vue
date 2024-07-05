7af21d45a1f43c43644809fd234b0e97d58372bd
sanggusseu <sanggusseu@gmail.com>
2024-07-06 07:08:24 +0900

    디렉티브

---
+++ src/lectures/12/App_directives.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>
+		<p>{{ msg }}</p>
+		<p v-text="msg"></p>
+		<p v-html="htmlStr"></p>
+		<p v-text="htmlStr"></p>
+		<p v-once>{{ msg }} !!!!</p>
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+	setup() {
+		/**
+		 * v-text는 html 태그 안에 textContent를 채운다.
+		 * v-html은 innerHTML과 비슷하게 태그를 인식한다.
+		 * v-once 엘리먼트와 컴포넌트를 한번(once)만 렌더링한다. 이후에 재렌더링 할 때
+		 * 엘리먼트/컴포넌트 및 모든 하위 엘리먼트는 정적 컨텐츠로 처리되고 건너뛴다.
+		 * 업데이트 성능을 최적화하는데 사용할 수 있다.
+		 */
+		const msg = ref('안녕하세요');
+		const htmlStr = ref('<strong>안녕~!!!</strong>');
+		return {
+			msg,
+			htmlStr,
+		};
+	},
+};
+</script>
+
+<style scoped></style>
---
+++ src/lectures/12/App_v-memo.vue
@@ -0,0 +1,51 @@
+<template>
+	<div>
+		<div v-once>
+			<h2>v-once</h2>
+			<p>subscribers: {{ subscribers }}</p>
+			<p>views: {{ views }}</p>
+			<p>likes: {{ likes }}</p>
+		</div>
+		<div v-memo="[subscribers]">
+			<h2>v-memo</h2>
+			<p>subscribers: {{ subscribers }}</p>
+			<p>views: {{ views }}</p>
+			<p>likes: {{ likes }}</p>
+		</div>
+		<div>
+			<h2>힣</h2>
+			<p>subscribers: {{ subscribers }}</p>
+			<p>views: {{ views }}</p>
+			<p>likes: {{ likes }}</p>
+		</div>
+		<button @click="subscribers++">Subs++</button>
+		<button @click="views++">Views++</button>
+		<button @click="likes++">Likes++</button>
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+	/**
+	 * v-memo 성능과 관련된 디렉티브로 배열 안에 반응형 데이터를 넣으면, 그 데이터가
+	 * 변경 됐을 때만 재렌더링 한다.
+	 * 성능최적화가 필요할 때만 사용한다.
+	 * ex) 리스트의 길이가 1000개가 넘어갈 때
+	 */
+	setup() {
+		const subscribers = ref(4000);
+		const views = ref(400);
+		const likes = ref(20);
+
+		return {
+			subscribers,
+			views,
+			likes,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
